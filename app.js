5328e20cc9460692d415c9d7dabc03f0ea4f5ce4
vrojas <victor253.rojas@gmail.com>
2021-04-20 22:35:30 -0500

    hash with md5

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+
 const app = express();
 
 
@@ -18,8 +19,6 @@ const userSchema =  new mongoose.Schema({
   password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -38,7 +37,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const user = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
 
   user.save(function(err){
@@ -52,7 +51,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username
-  const password = req.body.password
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundList) {
     if (err) {
